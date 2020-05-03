2b566e7ff2268dadc9b949def2d41e69acbe35b2
Lachlan Garrity <13561523@student.uts.edu.au>
2020-05-03 17:27:45 +1000

    Implementing new input system

---
+++ Nautical Prototype/Assets/Scripts/InstantiateWater.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateWater : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject toInstantiate;
+    void Start()
+    {
+        for(int i = 0; i < 200; i = i + 4){
+            for(int k = 0; k < 200; k = k + 4){
+                Instantiate(toInstantiate, new Vector3(i,k,0f), new Quaternion());
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
